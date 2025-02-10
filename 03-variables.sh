7457f1770247e2226bd9e425d247a493c0b7b970
samarareddy <samara.junna@gmail.com>
2025-02-10 12:16:33 +0530

    some-message

---
+++ 03-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#declaring of variable
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1 :: Hello $PERSON2 , How are you?"
+echo "$PERSON2 :: I am good, How are you?"
+echo "$PERSON1 :: I am good too, Thanks for asking"
+echo "$PERSON2 :: You are welcome"
