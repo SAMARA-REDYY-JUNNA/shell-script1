ab7e7e216ff78e9c0054f30a0cb15409f5fc177c
samarareddy <samara.junna@gmail.com>
2025-02-10 12:25:05 +0530

    some-message

---
+++ 04-varaibles.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+each "Please enter username::"
+
+read -s USERNAME #here USERNAME is a variable
+
+echo "Please enter password::"
+
+read -s PASSWORD #here PASSWORD is a variable
+
+echo "Username is: $USERNAME and Password is: $PASSWORD"
