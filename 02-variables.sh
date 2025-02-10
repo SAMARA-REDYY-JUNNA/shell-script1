d36ea031aa9d8729c51cdd95242935bb989e559e
samarareddy <samara.junna@gmail.com>
2025-02-10 12:10:42 +0530

    some-message

--- 02-variables.sh
+++ 02-variables.sh
@@ -3,7 +3,7 @@
 PERSON1=Sam
 PERSON2=Bindu
 
-echo "$PERSON1:: Hello $PERSON2 , How are you?"
-echo "PERSON2 :: I am good, How are you?"
-echo "PERSON1 :: I am good too, Thanks for asking"
-echo "PERSON2 :: You are welcome"
+echo "$PERSON1 :: Hello $PERSON2 , How are you?"
+echo "$PERSON2 :: I am good, How are you?"
+echo "$PERSON1 :: I am good too, Thanks for asking"
+echo "$PERSON2 :: You are welcome"
