5404f480979ca9cc5434000cc09d37378e70a602
samarareddy <samara.junna@gmail.com>
2025-02-10 11:38:26 +0530

    some-message

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-echp "hello world"
+
+echo "hello world"
--- 02-variables.sh
+++ 02-variables.sh
@@ -1,1 +1,6 @@
-hi im sam
+#!/bin/bash
+
+echo "sam :: Hello Bindu , How are you?"
+echo "bindu :: I am good, How are you?"
+echo "sam :: I am good too, Thanks for asking"
+echo "bindu :: You are welcome"
