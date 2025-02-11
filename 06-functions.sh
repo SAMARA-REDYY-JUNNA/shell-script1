0a6f4d12b048f85b7de53fcd0a7f6ce456e6e7df
samarareddy <samara.junna@gmail.com>
2025-02-11 11:07:55 +0530

    some-message

--- 06-functions.sh
+++ 06-functions.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 USERID=$(id -u)
-TIMESTAMP=$(date '+%Y%m%d%H%M%S')
+TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOGFILE=/tmp/$SCRIPT_NAME_$TIMESTAMP.log
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 
 VALIDATE(){
     if [ $1 -ne 0 ]
