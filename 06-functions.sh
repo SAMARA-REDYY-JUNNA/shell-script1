c603b83d25239f1fe421f23e005e994aa4e37316
samarareddy <samara.junna@gmail.com>
2025-02-11 11:04:05 +0530

    some-message

--- 06-functions.sh
+++ 06-functions.sh
@@ -20,7 +20,7 @@ then
     echo "Please run this script with root access."
     exit 1 # manually exit if error comes.
 else
-    echo "You are super user.
+    echo "You are super user."
 fi
 
 dnf install mysql -y $>> $LOGFILE
